3f72d523e5e3d0ea4f565869bc8a9b1d6865cb37
aardravinod5 <aardravinod5@gmail.com>
2026-03-28 14:25:37 +0530

    LEVEL 1

---
+++ LEVEL 1/SumCompareWhile.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumCompareWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            int sumWhile = 0;
+            int i = 1;
+
+            while (i <= n) {
+                sumWhile += i;
+                i++;
+            }
+
+            int sumFormula = n * (n + 1) / 2;
+
+            System.out.println("While sum = " + sumWhile);
+            System.out.println("Formula sum = " + sumFormula);
+
+            if (sumWhile == sumFormula)
+                System.out.println("Both results are correct");
+        }
+    }
+}
